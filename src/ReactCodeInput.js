9eb5858bb858496bdf0ba561e5c877b6d049bdad
Bill Bergquist <bill.bergquist@vatomic.io>
2018-09-06 13:24:29 -0700

    Added forceUppercase prop (#46)

--- src/ReactCodeInput.js
+++ src/ReactCodeInput.js
@@ -19,7 +19,12 @@ class ReactCodeInput extends Component {
   constructor(props) {
     super(props);
 
-    const { value, fields, type, isValid, disabled, filterKeyCodes } = props;
+    const { fields, type, isValid, disabled, filterKeyCodes, forceUppercase } = props;
+    let { value } = props;
+
+    if (forceUppercase) {
+      value = value.toUpperCase();
+    }
 
     this.state = {
       value,
@@ -82,6 +87,10 @@ class ReactCodeInput extends Component {
 
   handleChange(e) {
     let value = String(e.target.value);
+
+    if (this.props.forceUppercase) {
+      value = value.toUpperCase();
+    }
 
     if (this.state.type === 'number') {
       value = value.replace(/[^\d]/g, '');
@@ -277,6 +286,7 @@ ReactCodeInput.defaultProps = {
   autoFocus:      true,
   isValid:        true,
   disabled:       false,
+  forceUppercase: false,
   fields:         4,
   value:          '',
   type:           'text',
@@ -299,6 +309,7 @@ ReactCodeInput.propTypes = {
   inputStyle:        PropTypes.object,
   inputStyleInvalid: PropTypes.object,
   autoFocus:         PropTypes.bool,
+  forceUppercase:    PropTypes.bool,
   filterKeyCodes:    PropTypes.array,
   pattern:           PropTypes.string,
   inputMode:         PropTypes.oneOf([
